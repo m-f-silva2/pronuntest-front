bbf10845e53e70d2fd1b033031f27922b46558f6
Elkin <elkin070@gmail.com>
2024-06-05 14:42:38 -0500

    speech therapy

---
+++ src/app/modules/dashboard/pages/nft/nft-auctions-table-item/nft-auctions-table-item.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { CurrencyPipe } from '@angular/common';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { Nft } from '../models/nft';
+
+@Component({
+    selector: '[nft-auctions-table-item]',
+    templateUrl: './nft-auctions-table-item.component.html',
+    standalone: true,
+    imports: [AngularSvgIconModule, CurrencyPipe],
+})
+export class NftAuctionsTableItemComponent implements OnInit {
+  @Input() auction = <Nft>{};
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/modules/dashboard/pages/speech-therapy/speech-therapy.component.ts
+++ src/app/modules/dashboard/pages/speech-therapy/speech-therapy.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { NftHeaderComponent } from '../nft/nft-header/nft-header.component';
+import { NftDualCardComponent } from '../nft/nft-dual-card/nft-dual-card.component';
+import { NftSingleCardComponent } from '../nft/nft-single-card/nft-single-card.component';
+import { NftAuctionsTableComponent } from '../nft/nft-auctions-table/nft-auctions-table.component';
+import { NftChartCardComponent } from '../nft/nft-chart-card/nft-chart-card.component';
 
 @Component({
   selector: 'app-speech-therapy',
   standalone: true,
-  imports: [],
+  imports: [NftHeaderComponent,NftDualCardComponent,NftSingleCardComponent,NftChartCardComponent,NftAuctionsTableComponent,],
   templateUrl: './speech-therapy.component.html',
   styleUrl: './speech-therapy.component.css'
 })
 export class SpeechTherapyComponent {
-
+  nft: Array<any> = [];
+  constructor(){
+    this.nft = [
+      {
+        id: 34356771,
+        title: 'Girls of the Cartoon Universe',
+        creator: 'Jhon Doe',
+        instant_price: 4.2,
+        price: 187.47,
+        ending_in: '06h 52m 47s',
+        last_bid: 0.12,
+        image: './assets/images/img-01.jpg',
+        avatar: './assets/avatars/avt-01.jpg',
+      },
+      {
+        id: 34356772,
+        title: 'Pupaks',
+        price: 548.79,
+        last_bid: 0.35,
+        image: './assets/images/img-02.jpg',
+      },
+      {
+        id: 34356773,
+        title: 'Seeing Green collection',
+        price: 234.88,
+        last_bid: 0.15,
+        image: './assets/images/img-03.jpg',
+      },
+    ];
+  }
 }
