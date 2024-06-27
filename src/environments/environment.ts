30611a538fbaf83fe7c583d116c7b77117f0e40b
Elkin <elkin070@gmail.com>
2024-06-27 14:59:03 -0500

    correción build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,7 @@
 export const environment = {
     production: false,
     appVersion: '0.0.23',
-    baseApiBD: 'https://pronuntest-backend.vercel.app/api', 
-    /*baseApiBD: 'http://localhost:3000/api',*/
+    baseApiBD: 'http://localhost:3000/api',
     apimUrlModules: {
         auth: 'auth'
     }
