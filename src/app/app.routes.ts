53a17d893bbd8904d64cc38477a80d7292be29cb
unknown <mfsilva@unimayor.edu.co>
2024-07-06 10:20:16 -0500

    fix route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 import { IslandsComponent } from './modules/games/islands/islands.component';
 import { LevelsComponent } from './modules/games/levels/levels.component';
-import { ActivitiesComponent } from './modules/games/activities/activities.component';
+
 import { AuthGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
     { path: '', loadChildren: () => import('./modules/layout/layout.routes'), canActivate: [AuthGuard] },
     { path: 'auth', loadChildren: () => import('./modules/auth/auth.routes') },
     { path: 'games', component: IslandsComponent, canActivate: [AuthGuard]},
-    { path: 'games/island/:island', component: LevelsComponent, canActivate: [AuthGuard] },
-    { path: 'games/island/:island/:level', component: ActivitiesComponent, canActivate: [AuthGuard] },
+    //{ path: 'games/island/:island', component: LevelsComponent, canActivate: [AuthGuard] },
+    { path: 'games/island/:island/:level', component: LevelsComponent, canActivate: [AuthGuard] },
 ];
