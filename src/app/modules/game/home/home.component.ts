8f398e5424f18b1fcde0409903a0cad3ff1dcef4
unknown <mfsilva@unimayor.edu.co>
2024-09-06 10:52:10 -0500

    diseño games home

--- src/app/modules/game/home/home.component.ts
+++ src/app/modules/game/home/home.component.ts
@@ -10,8 +10,8 @@ import { RouterLink } from '@angular/router';
 })
 export class HomeComponent {
   phonemes = [
-    {name: 'pa', label:'Jugar', hideLock: true, link: '/games/island'},
-    {name: 'r', label:'', hideLock: false, link: '/games'},
+    {name: 'p', label:'Jugar', hideLock: true, link: '/games/island', styleClass: "-mb-8 block bg-green-600 h-fit z-10 text-2xl relative w-fit px-2 py-1 rounded-xl font-bold text-yellow-50 border-yellow-950 border-2"},
+    {name: 'r', label:'', hideLock: false, link: '/games', styleClass: "-mb-8 block bg-gray-500 h-fit z-10 text-2xl relative w-fit px-2 py-1 rounded-xl font-bold text-yellow-50 border-yellow-950 border-2"},
   ]
   constructor(){
   }
